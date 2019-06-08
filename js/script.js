320dca1dfb3959687e372f42112da467ab4034f8
yareyaska <uraharahussein@gmail.com>
2019-06-08 08:02:13 -0400

    modified script.js  page

--- js/script.js
+++ js/script.js
@@ -1,13 +1,39 @@
-
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $("#showing").Toggle();
-    $("#hidden").Toggle();
-
-    
+    $("#showing").slideToggle();
+    $("#hidden").slideToggle();
   });
 
+  $(".clickable2").click(function() {
+    $("#showing2").slideToggle();
+    $("#hidden2").slideToggle();
+  });
 
+  $(".clickable3").click(function() {
+    $("#showing3").slideToggle();
+    $("#hidden3").slideToggle();
+  });
+  $('.over').hover(function() {
+    $(this).animate({
+      opacity: '1'
+    });
+  },
+  function() {
+    $(this).animate( {
+      opacity : '0'
+    });
+  });
+  $("#submit").click(function(event){
+    var name = $("#fname").val();
+    if(name != ""){
+      alert(name + "thanks for sending a message.")
+
+    }else {
+      alert("please fill in your details")
+    }
+      event.preventDefault();
+    });
+});
 
 
     
