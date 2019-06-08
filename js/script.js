a981db63c25f1f945074255d8cd832fbe2e45ad8
yareyaska <uraharahussein@gmail.com>
2019-06-08 08:25:13 -0400

    modified script.js  page

--- js/script.js
+++ js/script.js
@@ -1,39 +1,38 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
-    $("#showing").slideToggle();
-    $("#hidden").slideToggle();
-  });
+	$(".clickable").click(function() {
+		$("#showing").slideToggle();
+		$("#hidden").slideToggle();
+	});
 
-  $(".clickable2").click(function() {
-    $("#showing2").slideToggle();
-    $("#hidden2").slideToggle();
-  });
+	$(".clickable2").click(function() {
+		$("#showing2").slideToggle();
+		$("#hidden2").slideToggle();
+	});
 
-  $(".clickable3").click(function() {
-    $("#showing3").slideToggle();
-    $("#hidden3").slideToggle();
-  });
-  $('.over').hover(function() {
-    $(this).animate({
-      opacity: '1'
-    });
-  },
-  function() {
-    $(this).animate( {
-      opacity : '0'
-    });
-  });
-  $("#submit").click(function(event){
-    var name = $("#fname").val();
-    if(name != ""){
-      alert(name + "thanks for sending a message.")
+	$(".clickable3").click(function() {
+		$("#showing3").slideToggle();
+		$("#hidden3").slideToggle();
+	});
+	$(".over").hover(function() {
+		$(this).animate({
+			opacity: '1'
+		});
+	},
+	function() {
+		$(this).animate( {
+			opacity : '0'
+		});
+	});
+	$("#submit").click(function(event){
+		var name = $("#fname").val();
+		if(name != ""){
+			alert(name + "thanks for sending a message.")
 
-    }else {
-      alert("please fill in your details")
-    }
-      event.preventDefault();
-    });
+		}else {
+			alert("please fill in your details")
+		}
+		event.preventDefault();
+	});
 });
 
 
-    
